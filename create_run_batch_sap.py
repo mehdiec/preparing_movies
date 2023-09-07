e964ab39f7d6a60688b683666b6774f808d88f33
mehdi <mehdiechh@gmail.com>
2023-09-07 17:06:55 +0200

    [+]

--- create_run_batch_sap.py
+++ create_run_batch_sap.py
@@ -1,4 +1,7 @@
 import os
+from pathlib import Path
+import argparse
+from create_sap_info import get_frame_ref_tr
 
 
 def generate_files(input_folder, output_folder):
@@ -20,30 +23,53 @@ def generate_files(input_folder, output_folder):
     input_files = list(os.listdir(input_folder))
     input_files.sort()
     content = ""
+    input_folder_path = Path(
+        "/Volumes/u934/equipe_bellaiche/m_ech-chouini/fb_analysis/substraction-RNAi/"
+    )
+    output_folder_path = input_folder_path / "SAP_info"
+
+    rescaling_file_path = "/path/to/rescaling/file.txt"  # Update with the actual path
+
+    # rescaling_data = read_rescaling_file(rescaling_file_path)
+
+    animals = [
+        file_name.split(".")[0]
+        for file_name in os.listdir(input_folder_path / Path("full_movies"))
+        if file_name.lower().endswith((".tif"))
+    ]
+
+    time_ref_dict = get_frame_ref_tr(input_folder_path, animals)
 
     for file_name in input_files:
-        print(file_name)
-        if not file_name.lower().endswith((".tif")):
-            continue
+        # if not file_name.lower().endswith((".tif")):
+        #     continue
         file_name = file_name.split(".")[0]
+        t_ref = time_ref_dict.get(file_name, 71)
+        start_frame = 1
+        # if t_ref > 10 and t_ref < 180:
+        #     continue
+        print(file_name)
         content += f"clear all; close all;\nSAP_info_{file_name}\n"
     output_file_path = os.path.join(output_folder, "run_batch.m")
+    print(content)
     with open(output_file_path, "w") as file:
         file.write(content)
 
 
 if __name__ == "__main__":
     # Replace 'input_folder_path' and 'output_folder_path' with the actual paths
-    input_folder_paths = [
-        "/Volumes/u934/equipe_bellaiche/m_ech-chouini/fb_analysis/substraction-RNAi/Dghetero_pnr-no80ts_dgRNAi/full_movies",
-        "/Volumes/u934/equipe_bellaiche/m_ech-chouini/fb_analysis/substraction-RNAi/Dghetero_pnr-no80ts_wRNAi/SAP_info",
-    ]
-    # input_folder_path = "/Volumes/u934/equipe_bellaiche/m_ech-chouini/fb_analysis"
-    output_folder_paths = [
-        "/Volumes/u934/equipe_bellaiche/m_ech-chouini/fb_analysis/substraction-RNAi/Dghetero_pnr-no80ts_dgRNAi/SAP_info",
-        "/Volumes/u934/equipe_bellaiche/m_ech-chouini/fb_analysis/substraction-RNAi/Dghetero_pnr-no80ts_wRNAi/SAP_info",
-    ]
-    for input_folder_path, output_folder_path in zip(
-        input_folder_paths, output_folder_paths
-    ):
-        generate_files(input_folder_path, output_folder_path)
+    parser = argparse.ArgumentParser(
+        description="CREATE ALL RUN BATCH!!!!!!",
+        epilog="hihi",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "parent_folder", type=Path, help="Path to folder containing the animal 1"
+    )
+
+    args = parser.parse_args()
+    input_folder_path = args.parent_folder / "full_movies"
+
+    output_folder_path = args.parent_folder / "SAP_info"
+
+    generate_files(input_folder_path, output_folder_path)
